4aa3c821bd95efe9da12d6f4e80253fb358a31f0
Tuan Vu Anh <vuanhtuanvn85@gmail.com>
2024-10-11 22:02:11 +0700

    add get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -74,8 +74,10 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log(books[req.params.isbn]);
+  
+  return res
+    .send(books[req.params.isbn].reviews);
 });
 
 module.exports.general = public_users;
